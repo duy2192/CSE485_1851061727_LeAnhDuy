887cdb05e88ca4e557869aeee435bc40d91cfe41
duy2192 <1851061727@e.tlu.edu.vn>
2021-01-05 13:26:55 +0700

    Start

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-secondary text-uppercase  " id="mainNav">
+            <div class="container"><a class="navbar-brand js-scroll-trigger" href="#page-top">CV</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#home">Home</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container-fluid main-section">
+            <div class="row ">
+                <div class="col-md-4 main-profile" >
+                    <img src="image/profile.png" alt="" class="img img-fluid d-block mx-auto mb-5 ">
+                </div>
+                <div class="col-md-8 col" >
+                    <section id="home" class="">
+                        <div class="container">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.                        
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia error vitae cum harum voluptas, laboriosam et nobis porro necessitatibus sint deserunt excepturi. Culpa voluptate magni illum beatae optio, totam est?
+                        </div>
+                    </section>
+                    <section id="about" class="">
+                        <div class="container">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem magnam perspiciatis ipsam doloremque tempore ex, porro iusto iure magni quod iste repellendus perferendis maiores officiis, neque voluptates ratione culpa voluptas.
+                      
+                        </div>
+                    </section>
+
+                </div>
+            </div>
+
+        </div>
+    </main>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="assets/js/freelancer.js"></script>
+</body>
+
+</html>
